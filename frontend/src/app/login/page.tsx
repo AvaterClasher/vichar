2f327ec4a16c1658041eba763fa7d17bcd990145
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-11-22 00:30:08 +0530

    feat: added pages for login and signup

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { LoginForm } from "@/components/login/login-comp";
+
+export default function Page() {
+	return (
+		<>
+			<div className="flex w-full h-screen items-center justify-center">
+				<LoginForm />
+			</div>
+		</>
+	);
+}
---
+++ frontend/src/app/signup/page.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import { SignUpForm } from "@/components/login/signup-comp";
+
+export default function Page() {
+	return (
+		<>
+			<div className="flex w-full h-screen items-center justify-center">
+				<SignUpForm />
+			</div>
+		</>
+	);
+}
