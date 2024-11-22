15eae5d7ec3c2a8ea931c13b6f666fc91d792d87
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-11-23 01:02:11 +0530

    fix: Code theming stops working on vercel need to make this serverside

--- frontend/src/components/codeBlock.tsx
+++ frontend/src/components/codeBlock.tsx
@@ -3,8 +3,6 @@
 import React from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { a11yDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 type MarkdownRendererProps = {
 	children: string;
@@ -13,31 +11,5 @@ type MarkdownRendererProps = {
 export function MarkdownRenderer({
 	children: markdown,
 }: MarkdownRendererProps) {
-	return (
-		<Markdown
-			remarkPlugins={[remarkGfm]}
-			components={{
-				code({ node, inline, className, children, ...props }: any) {
-					const match = /language-(\w+)/.exec(className || "");
-
-					return !inline && match ? (
-						<SyntaxHighlighter
-							style={a11yDark}
-							PreTag="div"
-							language={match[1]}
-							showLineNumbers={true}
-							wrapLines={true}
-							{...props}>
-							{String(children).replace(/\n$/, "")}
-						</SyntaxHighlighter>
-					) : (
-						<code className={className} {...props}>
-							{children}
-						</code>
-					);
-				},
-			}}>
-			{markdown}
-		</Markdown>
-	);
+	return <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>;
 }
