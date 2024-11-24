411c277be6a46676f493ff6da1742d14dd70f25c
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-11-24 23:16:32 +0530

    feat: hey added swagger scheme to generate based on prod or not

--- backend/src/swagger.js
+++ backend/src/swagger.js
@@ -5,7 +5,8 @@ const doc = {
     title: 'Blog API',
     description: 'API documentation for the Blog API',
   },
-  host: process.env.NODE_ENV === 'production' ? 'https://vichar-qf8u.onrender.com' : `localhost:${process.env.PORT || 5000}`
+  schemes: [process.env.NODE_ENV === 'production' ? 'https' : 'http'],
+  host: process.env.NODE_ENV === 'production' ? 'vichar-qf8u.onrender.com' : `localhost:${process.env.PORT || 5000}`
 };
 
 console.log(doc.host)
